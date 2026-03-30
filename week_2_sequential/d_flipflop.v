977a4055c065e33661602839fd39fb30787e517a
zero-odds <your-github-email@gmail.com>
2026-03-31 00:08:33 +0530

    organised week 1 files and added week 2 D flip flop

---
+++ week_2_sequential/d_flipflop.v
@@ -0,0 +1,24 @@
+module d_ff(input clk,d,output reg q);
+always @(posedge clk) begin
+q<=d;
+end
+endmodule 
+module tb;
+reg clk,d;
+wire q;
+d_ff uut(.clk(clk),.d(d),.q(q));
+initial begin
+clk=0;
+forever #5 clk = ~clk;
+end
+initial begin
+d=0;
+#12 d=1;
+#15 d=0;
+#20 d=1;
+#100 $finish;
+end
+initial begin
+$monitor("time=%0t | clk= %b | d=%b | q=%b",$time,clk,d,q);
+end
+endmodule
